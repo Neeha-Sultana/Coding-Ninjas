aa4e331607a5bfb5bfaf51c9c38982a6e7b72586
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2023-11-30 08:41:41 +0530

    Create Data_Type.py

---
+++ Data_Type.py
@@ -0,0 +1,16 @@
+from typing import *
+
+def dataTypes(type: str):
+    if type=='Long':
+        return 8
+    elif type=='Integer':
+        return 4
+    elif type=='Float':
+        return 4
+    elif type=='Double':
+        return 8
+    elif type=='Character':
+        return 1
+
+    type=input()
+    print(dataTypes(type))
