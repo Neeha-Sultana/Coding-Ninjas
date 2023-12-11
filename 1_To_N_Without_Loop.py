3d9ef0ccfe71172504f90194ab3c4d80ca5cf05a
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2023-12-11 23:13:55 +0530

    Create 1_To_N_Without_Loop.py

---
+++ 1_To_N_Without_Loop.py
@@ -0,0 +1,11 @@
+from typing import List
+l=list()
+def printNos(x: int) -> List[int]: 
+    # Write your code here
+    if x<1:
+        return
+    else:
+        #l.append(x)
+        printNos(x-1)
+        l.append(x)
+    return l
