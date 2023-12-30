8b9a26561d1d1a279f1859d99c9986417808c606
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2023-12-30 22:40:59 +0530

    Create  Factorial_Numbers_Not_Greater_Than_N.py

---
+++ Factorial_Numbers_Not_Greater_Than_N.py
@@ -0,0 +1,39 @@
+from typing import *
+
+ 
+
+def factorialNumbers(n: int) -> List[int]:
+
+    # Write your code here
+
+    srr=[]
+
+    s=1
+
+    array_fact(s,n,srr)
+
+    return srr
+
+ 
+
+def array_fact(fact_num,original_num,arr):
+
+    if return_fact(fact_num)>original_num:
+
+        return arr
+
+    arr.append(return_fact(fact_num))
+
+    fact_num+=1
+
+    array_fact(fact_num,original_num,arr)
+
+ 
+
+def return_fact(n):
+
+    if n==1:
+
+        return 1
+
+    return n * return_fact(n-1)
