3a79d0f61264ff4f24d5656afa8f4f056511e6ae
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-05 20:15:54 +0530

    Create Bubble_Sort.py

---
+++ Bubble_Sort.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def bubbleSort(arr: List[int], n: int):
+    # Your code goes here.
+    for i in range(n-1, 0, -1):
+        swapped = False
+        for j in range(0, i):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+
+        if not swapped:
+            break        
+
+
+    return arr 
