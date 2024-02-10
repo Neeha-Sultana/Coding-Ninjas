8681b5a7a42774965b5944fb655bb27d4a8a8ba4
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-10 09:15:04 +0530

    Create Largest_Element_In_The_Array.py

---
+++ Largest_Element_In_The_Array.py
@@ -0,0 +1,11 @@
+from sys import *
+from collections import *
+from math import *
+
+def largestElement(arr: [], n: int) -> int:
+    lar=arr[0]
+    for i in range(1,len(arr)):
+        if arr[i]>lar:
+            lar=arr[i]
+
+    return lar
