d4e6481476f1d069be1e10bfaae2885892519798
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-11 10:55:15 +0530

    Create Check_Sorted_array.py

---
+++ Check_Sorted_array.py
@@ -0,0 +1,14 @@
+def isSorted(n: int,  a: [int]) -> int:
+    # Write youcode here.
+  
+    for i in range(1,len(a)):
+        if a[i-1]>a[i]:
+            x=False
+            break
+        
+        x=True
+        
+    if x==True:
+        return 1
+    else:
+        return 0
