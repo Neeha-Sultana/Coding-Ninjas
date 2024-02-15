2b74ca4403c17bf334693d15bded56f75a31239d
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-02-15 18:51:48 +0530

    Create Find_The_Single_Element.py

---
+++ Find_The_Single_Element.py
@@ -0,0 +1,14 @@
+from typing import *
+
+def getSingleElement(arr : List[int]) -> int:
+    dict1={}
+    for i in range(len(arr)):
+        if arr[i] not in dict1:
+            dict1[arr[i]]=1
+        else:
+            dict1[arr[i]]+=1
+
+    
+    for key,values in dict1.items():
+        if values==1:
+            return key
