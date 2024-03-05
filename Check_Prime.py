6d654379e26f3956f97995f13d8dc96395215f20
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-05 23:12:13 +0530

    Update Check_Prime.py

--- Check_Prime.py
+++ Check_Prime.py
@@ -4,13 +4,11 @@ from sys import *
 from os import *
 n = int(input())
 counti = 0
-
 # Loop until the square root of n
 for i in range(2, int((n**0.5) + 1)):
     if n % i == 0:
         counti += 1
         break  # If a factor is found, no need to continue checking
-
 if counti == 0 and n > 1:
     print("YES")  # Prime number has only 2 factors: 1 and itself
 else:
