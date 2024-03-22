dd60e0ff146ca18ec20dcad77b4ec085c7a61ed9
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-22 23:04:04 +0530

    Add files via upload

---
+++ Best_Time_to_Buy_and_Sell _Stock.py
@@ -0,0 +1,22 @@
+"""
+#BruteForce
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        diff=0
+        for i in range(len(prices)):
+            for j in range(i+1,len(prices)):
+                if prices[i]<prices[j]:
+                    if (prices[j]-prices[i])>diff:
+                        diff=(prices[j]-prices[i])
+                        
+        return diff
+"""
+#Optimal
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxPro = 0
+        minPrice = float('inf')
+        for i in range(len(prices)):
+            minPrice = min(minPrice, prices[i])
+            maxPro = max(maxPro, prices[i] - minPrice)
+        return maxPro
