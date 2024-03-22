cdd0934b340e9fc5be85b21bebcffa0e46fa6b55
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-03-22 23:04:56 +0530

    Add files via upload

---
+++ Rotate_Matrix.py
@@ -0,0 +1,37 @@
+'''
+#BruteForce
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n=len(matrix)
+        rot=[[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                rot[j][n-i-1]=matrix[i][j]
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j]=rot[i][j]
+'''
+
+
+
+
+#optimal
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n=len(matrix)
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j],matrix[j][i]=matrix[j][i],matrix[i][j]
+                
+        for i in range(n):
+            left=0
+            right=n-1
+            while(left<right):
+                matrix[i][left],matrix[i][right]=matrix[i][right],matrix[i][left]
+                left+=1
+                right-=1
+        
+        
