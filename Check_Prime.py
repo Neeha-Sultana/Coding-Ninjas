43e8394ab81a85dc10a526db7e788df454587b14
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-05-28 23:55:39 +0530

    Update Check_Prime.py

--- Check_Prime.py
+++ Check_Prime.py
@@ -8,6 +8,8 @@ counti = 0
 for i in range(2, int((n**0.5) + 1)):
     if n % i == 0:        
         counti += 1
+
+        
         break  # If a factor is found, no need to continue checking
 if counti == 0 and n > 1:
     print("YES")  # Prime number has only 2 factors: 1 and itself
