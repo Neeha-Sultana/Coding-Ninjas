05c43b0398ed034c8d5c47a35148b4c7aba7bf39
Neeha-Sultana <104249476+Neeha-Sultana@users.noreply.github.com>
2024-05-28 23:55:31 +0530

    Update Check_Prime.py

--- Check_Prime.py
+++ Check_Prime.py
@@ -6,8 +6,7 @@ n = int(input())
 counti = 0
 # Loop until the square root of n
 for i in range(2, int((n**0.5) + 1)):
-    if n % i == 0:
-        
+    if n % i == 0:        
         counti += 1
         break  # If a factor is found, no need to continue checking
 if counti == 0 and n > 1:
